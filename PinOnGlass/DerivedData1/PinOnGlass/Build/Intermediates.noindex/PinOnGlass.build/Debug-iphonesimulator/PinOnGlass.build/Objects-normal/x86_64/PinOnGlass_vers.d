505f4a1339b6909e037774f3ada6e377b45e9fa0
Lachekumar Nadar <lachhekumar.nadar@nttdata.com>
2019-09-10 11:02:24 +0530

    reseting Demo App

--- PinOnGlass/DerivedData1/PinOnGlass/Build/Intermediates.noindex/PinOnGlass.build/Debug-iphonesimulator/PinOnGlass.build/Objects-normal/x86_64/PinOnGlass_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/user914766/Documents/PinOnGlass/PinOnGlass/DerivedData1/PinOnGlass/Build/Intermediates.noindex/PinOnGlass.build/Debug-iphonesimulator/PinOnGlass.build/DerivedSources/PinOnGlass_vers.c
