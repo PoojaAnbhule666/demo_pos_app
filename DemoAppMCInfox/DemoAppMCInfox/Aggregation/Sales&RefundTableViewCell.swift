4b3adca437a65b0a6f185cc5c289a7c10c2fae52
Lachekumar Nadar <lachhekumar.nadar@nttdata.com>
2019-09-11 16:06:15 +0530

    Degin the cell of  AggregationVc

---
+++ DemoAppMCInfox/DemoAppMCInfox/Aggregation/Sales&RefundTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Sales&RefundTableViewCell.swift
+//  DemoAppMCInfox
+//
+//  Created by CAFIS_Mac_1 on 11/09/19.
+//  Copyright © 2019 NTTDATA_Cafis. All rights reserved.
+//
+
+import UIKit
+
+class Sales_RefundTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
