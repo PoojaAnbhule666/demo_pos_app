a0c3f15c314835eac74571a7e24ddc9a8d705f4a
Lachekumar Nadar <lachhekumar.nadar@nttdata.com>
2019-09-12 10:26:02 +0530

    commit add new cell for aggregat by deepak

--- DemoAppMCInfox/DemoAppMCInfox/Aggregation/AggregationViewController.swift
+++ DemoAppMCInfox/DemoAppMCInfox/Aggregation/AggregationViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class AggregationViewController: UIViewController {
-
+    var productArray = [
+        ["Price" : "10000" ,"Product Name" : "Success : AMEX CARD"],
+        ["Price" : "500" ,"Product Name" : "Success : VISA CARD"],
+        ["Price" : "600" ,"Product Name" : "Success : MASTER CARD"],
+        ["Price" : "700" ,"Product Name" : "Success : JCB CARD"],
+        ["Price" : "900" ,"Product Name" : "Success : DINERS CARD"],
+        ["Price" : "1000" ,"Product Name" : "Rejected by issuer"],
+        ["Price" : "2000" ,"Product Name" : "Error from CAFIS server"],
+        ["Price" : "3000" ,"Product Name" : "System Error : CAFIS GW"],
+        ["Price" : "4000" ,"Product Name" : "Success : E211 received"],
+        ["Price" : "5000" ,"Product Name" : "Failure : E212 received"],
+        ["Price" : "5500" ,"Product Name" : "Success : E212 received"],
+        ["Price" : "6000" ,"Product Name" : "Timeout : CAFIS server"],
+        ["Price" : "1100" ,"Product Name" : "Card rejected by issuer"],
+        ["Price" : "2100" ,"Product Name" : "Card error from CAFIS"],
+        ["Price" : "3100" ,"Product Name" : "CAFIS sys error"]
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
