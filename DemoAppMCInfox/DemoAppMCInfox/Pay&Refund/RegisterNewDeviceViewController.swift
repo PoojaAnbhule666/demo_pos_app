574100377bb5947e1abff1b9355605dded78a96c
Lachhe <lachhekumar.nadar@nttdata.com>
2019-09-20 14:19:13 +0530

    device register coding - by shivani

---
+++ DemoAppMCInfox/DemoAppMCInfox/Pay&Refund/RegisterNewDeviceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterNewDeviceViewController.swift
+//  DemoAppMCInfox
+//
+//  Created by Lachhekumar Nadar on 9/20/19.
+//  Copyright © 2019 NTTDATA_Cafis. All rights reserved.
+//
+
+import UIKit
+
+class RegisterNewDeviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
